xintongsong/flink-1

[FLINK-14584][python] Support MapType

flink-python/pyflink/fn_execution/coder_impl.py
@@ -103,6 +103,40 @@ class ArrayCoderImpl(StreamCoderImpl):
         return 'ArrayCoderImpl[%s]' % str(self._elem_coder)
 
 
+class MapCoderImpl(StreamCoderImpl):
+
+    def __init__(self, key_coder, value_coder):
+        self._key_coder = key_coder
+        self._value_coder = value_coder
+
+    def encode_to_stream(self, map_value, out_stream, nested):
+        out_stream.write_bigendian_int32(len(map_value))
+        for key in map_value:
+            self._key_coder.encode_to_stream(key, out_stream, nested)
+            value = map_value[key]
+            if value is None:
+                out_stream.write_byte(True)
+            else:
+                out_stream.write_byte(False)
+                self._value_coder.encode_to_stream(map_value[key], out_stream, nested)
+
+    def decode_from_stream(self, in_stream, nested):
+        size = in_stream.read_bigendian_int32()
+        map_value = {}
+        for _ in range(size):
+            key = self._key_coder.decode_from_stream(in_stream, nested)
+            is_null = not not in_stream.read_byte()
+            if is_null:
+                map_value[key] = None
+            else:
+                value = self._value_coder.decode_from_stream(in_stream, nested)
+                map_value[key] = value
+        return map_value
+
+    def __repr__(self):
+        return 'MapCoderImpl[%s]' % ' : '.join([str(self._key_coder), str(self._value_coder)])
+
+
 class BigIntCoderImpl(StreamCoderImpl):
     def encode_to_stream(self, value, out_stream, nested):
         out_stream.write_bigendian_int64(value)

flink-python/pyflink/fn_execution/coders.py
@@ -31,7 +31,8 @@ FLINK_SCHEMA_CODER_URN = "flink:coder:schema:v1"
 
 __all__ = ['RowCoder', 'BigIntCoder', 'TinyIntCoder', 'BooleanCoder',
            'SmallIntCoder', 'IntCoder', 'FloatCoder', 'DoubleCoder',
-           'BinaryCoder', 'CharCoder', 'DateCoder', 'ArrayCoder']
+           'BinaryCoder', 'CharCoder', 'DateCoder', 'ArrayCoder',
+           'MapCoder']
 
 
 class RowCoder(FastCoder):
@@ -112,6 +113,39 @@ class ArrayCoder(CollectionCoder):
 
     def _impl_coder(self):
         return coder_impl.ArrayCoderImpl
+
+
+class MapCoder(FastCoder):
+    """
+    Coder for Map.
+    """
+
+    def __init__(self, key_coder, value_coder):
+        self._key_coder = key_coder
+        self._value_coder = value_coder
+
+    def _create_impl(self):
+        return coder_impl.MapCoderImpl(self._key_coder.get_impl(), self._value_coder.get_impl())
+
+    def is_deterministic(self):
+        return self._key_coder.is_deterministic() and self._value_coder.is_deterministic()
+
+    def to_type_hint(self):
+        return {}
+
+    def __repr__(self):
+        return 'MapCoder[%s]' % ','.join([str(self._key_coder), str(self._value_coder)])
+
+    def __eq__(self, other):
+        return (self.__class__ == other.__class__
+                and self._key_coder == other._key_coder
+                and self._value_coder == other._value_coder)
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __hash__(self):
+        return hash([self._key_coder, self._value_coder])
 
 
 class DeterministicCoder(FastCoder, ABC):
@@ -279,5 +313,8 @@ def from_proto(field_type):
         return RowCoder([from_proto(f.type) for f in field_type.row_schema.fields])
     elif field_type_name == type_name.ARRAY:
         return ArrayCoder(from_proto(field_type.collection_element_type))
+    elif field_type_name == type_name.MAP:
+        return MapCoder(from_proto(field_type.map_type.key_type),
+                        from_proto(field_type.map_type.value_type))
     else:
         raise ValueError("field_type %s is not supported." % field_type)

flink-python/src/main/java/org/apache/flink/table/runtime/typeutils/PythonTypeUtils.java
@@ -27,12 +27,14 @@ import org.apache.flink.api.common.typeutils.base.FloatSerializer;
 import org.apache.flink.api.common.typeutils.base.GenericArraySerializer;
 import org.apache.flink.api.common.typeutils.base.IntSerializer;
 import org.apache.flink.api.common.typeutils.base.LongSerializer;
+import org.apache.flink.api.common.typeutils.base.MapSerializer;
 import org.apache.flink.api.common.typeutils.base.ShortSerializer;
 import org.apache.flink.api.common.typeutils.base.array.BytePrimitiveArraySerializer;
 import org.apache.flink.api.java.typeutils.runtime.RowSerializer;
 import org.apache.flink.fnexecution.v1.FlinkFnApi;
 import org.apache.flink.table.runtime.typeutils.serializers.python.BaseRowSerializer;
 import org.apache.flink.table.runtime.typeutils.serializers.python.BinaryArraySerializer;
+import org.apache.flink.table.runtime.typeutils.serializers.python.BinaryMapSerializer;
 import org.apache.flink.table.runtime.typeutils.serializers.python.DateSerializer;
 import org.apache.flink.table.runtime.typeutils.serializers.python.StringSerializer;
 import org.apache.flink.table.types.logical.ArrayType;
@@ -45,6 +47,7 @@ import org.apache.flink.table.types.logical.DoubleType;
 import org.apache.flink.table.types.logical.FloatType;
 import org.apache.flink.table.types.logical.IntType;
 import org.apache.flink.table.types.logical.LogicalType;
+import org.apache.flink.table.types.logical.MapType;
 import org.apache.flink.table.types.logical.RowType;
 import org.apache.flink.table.types.logical.SmallIntType;
 import org.apache.flink.table.types.logical.TimeType;
@@ -188,6 +191,13 @@ public final class PythonTypeUtils {
 		}
 
 		@Override
+		public TypeSerializer visit(MapType mapType) {
+			TypeSerializer<?> keyTypeSerializer = mapType.getKeyType().accept(this);
+			TypeSerializer<?> valueTypeSerializer = mapType.getValueType().accept(this);
+			return new MapSerializer<>(keyTypeSerializer, valueTypeSerializer);
+		}
+
+		@Override
 		public TypeSerializer visit(RowType rowType) {
 			final TypeSerializer[] fieldTypeSerializers = rowType.getFields()
 				.stream()
@@ -234,6 +244,15 @@ public final class PythonTypeUtils {
 			LogicalType elementType = arrayType.getElementType();
 			TypeSerializer<?> elementTypeSerializer = elementType.accept(this);
 			return new BinaryArraySerializer<>(elementType, elementTypeSerializer);
+		}
+
+		@Override
+		public TypeSerializer visit(MapType mapType) {
+			LogicalType keyType = mapType.getKeyType();
+			LogicalType valueType = mapType.getValueType();
+			TypeSerializer<?> keyTypeSerializer = keyType.accept(this);
+			TypeSerializer<?> valueTypeSerializer = valueType.accept(this);
+			return new BinaryMapSerializer<>(keyType, valueType, keyTypeSerializer, valueTypeSerializer);
 		}
 	}
 
@@ -343,6 +362,21 @@ public final class PythonTypeUtils {
 
 			FlinkFnApi.Schema.FieldType elementFieldType = arrayType.getElementType().accept(this);
 			builder.setCollectionElementType(elementFieldType);
+			return builder.build();
+		}
+
+		@Override
+		public FlinkFnApi.Schema.FieldType visit(MapType mapType) {
+			FlinkFnApi.Schema.FieldType.Builder builder =
+				FlinkFnApi.Schema.FieldType.newBuilder()
+					.setTypeName(FlinkFnApi.Schema.TypeName.MAP)
+					.setNullable(mapType.isNullable());
+
+			FlinkFnApi.Schema.MapType.Builder mapBuilder =
+				FlinkFnApi.Schema.MapType.newBuilder()
+					.setKeyType(mapType.getKeyType().accept(this))
+					.setValueType(mapType.getValueType().accept(this));
+			builder.setMapType(mapBuilder.build());
 			return builder.build();
 		}
 

hequn8128/flink

flink-python/pyflink/fn_execution/coder_impl.py
@@ -103,6 +103,40 @@ class ArrayCoderImpl(StreamCoderImpl):
         return 'ArrayCoderImpl[%s]' % repr(self._elem_coder)
 
 
+class MapCoderImpl(StreamCoderImpl):
+
+    def __init__(self, key_coder, value_coder):
+        self._key_coder = key_coder
+        self._value_coder = value_coder
+
+    def encode_to_stream(self, map_value, out_stream, nested):
+        out_stream.write_bigendian_int32(len(map_value))
+        for key in map_value:
+            self._key_coder.encode_to_stream(key, out_stream, nested)
+            value = map_value[key]
+            if value is None:
+                out_stream.write_byte(True)
+            else:
+                out_stream.write_byte(False)
+                self._value_coder.encode_to_stream(map_value[key], out_stream, nested)
+
+    def decode_from_stream(self, in_stream, nested):
+        size = in_stream.read_bigendian_int32()
+        map_value = {}
+        for _ in range(size):
+            key = self._key_coder.decode_from_stream(in_stream, nested)
+            is_null = in_stream.read_byte()
+            if is_null:
+                map_value[key] = None
+            else:
+                value = self._value_coder.decode_from_stream(in_stream, nested)
+                map_value[key] = value
+        return map_value
+
+    def __repr__(self):
+        return 'MapCoderImpl[%s]' % ' : '.join([repr(self._key_coder), repr(self._value_coder)])
+
+
 class BigIntCoderImpl(StreamCoderImpl):
     def encode_to_stream(self, value, out_stream, nested):
         out_stream.write_bigendian_int64(value)

flink-python/pyflink/fn_execution/coders.py
@@ -32,7 +32,7 @@ FLINK_SCHEMA_CODER_URN = "flink:coder:schema:v1"
 __all__ = ['RowCoder', 'BigIntCoder', 'TinyIntCoder', 'BooleanCoder',
            'SmallIntCoder', 'IntCoder', 'FloatCoder', 'DoubleCoder',
            'BinaryCoder', 'CharCoder', 'DateCoder', 'TimeCoder',
-           'TimestampCoder', 'ArrayCoder']
+           'TimestampCoder', 'ArrayCoder', 'MapCoder']
 
 
 class RowCoder(FastCoder):
@@ -110,6 +110,39 @@ class ArrayCoder(CollectionCoder):
 
     def _create_impl(self):
         return coder_impl.ArrayCoderImpl(self._elem_coder.get_impl())
+
+
+class MapCoder(FastCoder):
+    """
+    Coder for Map.
+    """
+
+    def __init__(self, key_coder, value_coder):
+        self._key_coder = key_coder
+        self._value_coder = value_coder
+
+    def _create_impl(self):
+        return coder_impl.MapCoderImpl(self._key_coder.get_impl(), self._value_coder.get_impl())
+
+    def is_deterministic(self):
+        return self._key_coder.is_deterministic() and self._value_coder.is_deterministic()
+
+    def to_type_hint(self):
+        return {}
+
+    def __repr__(self):
+        return 'MapCoder[%s]' % ','.join([repr(self._key_coder), repr(self._value_coder)])
+
+    def __eq__(self, other):
+        return (self.__class__ == other.__class__
+                and self._key_coder == other._key_coder
+                and self._value_coder == other._value_coder)
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __hash__(self):
+        return hash([self._key_coder, self._value_coder])
 
 
 class DeterministicCoder(FastCoder, ABC):
@@ -307,5 +340,8 @@ def from_proto(field_type):
         return TimestampCoder(field_type.date_time_type.precision)
     elif field_type_name == type_name.ARRAY:
         return ArrayCoder(from_proto(field_type.collection_element_type))
+    elif field_type_name == type_name.MAP:
+        return MapCoder(from_proto(field_type.map_type.key_type),
+                        from_proto(field_type.map_type.value_type))
     else:
         raise ValueError("field_type %s is not supported." % field_type)

flink-python/src/main/java/org/apache/flink/table/runtime/typeutils/PythonTypeUtils.java
@@ -27,11 +27,13 @@ import org.apache.flink.api.common.typeutils.base.FloatSerializer;
 import org.apache.flink.api.common.typeutils.base.GenericArraySerializer;
 import org.apache.flink.api.common.typeutils.base.IntSerializer;
 import org.apache.flink.api.common.typeutils.base.LongSerializer;
+import org.apache.flink.api.common.typeutils.base.MapSerializer;
 import org.apache.flink.api.common.typeutils.base.ShortSerializer;
 import org.apache.flink.api.common.typeutils.base.array.BytePrimitiveArraySerializer;
 import org.apache.flink.api.java.typeutils.runtime.RowSerializer;
 import org.apache.flink.fnexecution.v1.FlinkFnApi;
 import org.apache.flink.table.runtime.typeutils.serializers.python.BaseArraySerializer;
+import org.apache.flink.table.runtime.typeutils.serializers.python.BaseMapSerializer;
 import org.apache.flink.table.runtime.typeutils.serializers.python.BaseRowSerializer;
 import org.apache.flink.table.runtime.typeutils.serializers.python.DateSerializer;
 import org.apache.flink.table.runtime.typeutils.serializers.python.StringSerializer;
@@ -47,6 +49,7 @@ import org.apache.flink.table.types.logical.DoubleType;
 import org.apache.flink.table.types.logical.FloatType;
 import org.apache.flink.table.types.logical.IntType;
 import org.apache.flink.table.types.logical.LogicalType;
+import org.apache.flink.table.types.logical.MapType;
 import org.apache.flink.table.types.logical.RowType;
 import org.apache.flink.table.types.logical.SmallIntType;
 import org.apache.flink.table.types.logical.TimeType;
@@ -200,6 +203,13 @@ public final class PythonTypeUtils {
 		}
 
 		@Override
+		public TypeSerializer visit(MapType mapType) {
+			TypeSerializer<?> keyTypeSerializer = mapType.getKeyType().accept(this);
+			TypeSerializer<?> valueTypeSerializer = mapType.getValueType().accept(this);
+			return new MapSerializer<>(keyTypeSerializer, valueTypeSerializer);
+		}
+
+		@Override
 		public TypeSerializer visit(RowType rowType) {
 			final TypeSerializer[] fieldTypeSerializers = rowType.getFields()
 				.stream()
@@ -255,6 +265,15 @@ public final class PythonTypeUtils {
 			LogicalType elementType = arrayType.getElementType();
 			TypeSerializer elementTypeSerializer = elementType.accept(this);
 			return new BaseArraySerializer(elementType, elementTypeSerializer);
+		}
+
+		@Override
+		public TypeSerializer visit(MapType mapType) {
+			LogicalType keyType = mapType.getKeyType();
+			LogicalType valueType = mapType.getValueType();
+			TypeSerializer<?> keyTypeSerializer = keyType.accept(this);
+			TypeSerializer<?> valueTypeSerializer = valueType.accept(this);
+			return new BaseMapSerializer(keyType, valueType, keyTypeSerializer, valueTypeSerializer);
 		}
 	}
 
@@ -386,6 +405,21 @@ public final class PythonTypeUtils {
 
 			FlinkFnApi.Schema.FieldType elementFieldType = arrayType.getElementType().accept(this);
 			builder.setCollectionElementType(elementFieldType);
+			return builder.build();
+		}
+
+		@Override
+		public FlinkFnApi.Schema.FieldType visit(MapType mapType) {
+			FlinkFnApi.Schema.FieldType.Builder builder =
+				FlinkFnApi.Schema.FieldType.newBuilder()
+					.setTypeName(FlinkFnApi.Schema.TypeName.MAP)
+					.setNullable(mapType.isNullable());
+
+			FlinkFnApi.Schema.MapType.Builder mapBuilder =
+				FlinkFnApi.Schema.MapType.newBuilder()
+					.setKeyType(mapType.getKeyType().accept(this))
+					.setValueType(mapType.getValueType().accept(this));
+			builder.setMapType(mapBuilder.build());
 			return builder.build();
 		}
 