xintongsong/flink-1

[FLINK-14584][python] Support ArrayType

flink-python/pyflink/fn_execution/coder_impl.py
@@ -79,6 +79,30 @@ class RowCoderImpl(StreamCoderImpl):
         return 'RowCoderImpl[%s]' % ', '.join(str(c) for c in self._field_coders)
 
 
+class ArrayCoderImpl(StreamCoderImpl):
+
+    def __init__(self, elem_coder):
+        self._elem_coder = elem_coder
+
+    def encode_to_stream(self, value, out_stream, nested):
+        out_stream.write_bigendian_int32(len(value))
+        for elem in value:
+            if elem is None:
+                out_stream.write_byte(False)
+            else:
+                out_stream.write_byte(True)
+                self._elem_coder.encode_to_stream(elem, out_stream, nested)
+
+    def decode_from_stream(self, in_stream, nested):
+        size = in_stream.read_bigendian_int32()
+        elements = [self._elem_coder.decode_from_stream(in_stream, nested)
+                    if not not in_stream.read_byte() else None for _ in range(size)]
+        return elements
+
+    def __repr__(self):
+        return 'ArrayCoderImpl[%s]' % str(self._elem_coder)
+
+
 class BigIntCoderImpl(StreamCoderImpl):
     def encode_to_stream(self, value, out_stream, nested):
         out_stream.write_bigendian_int64(value)

hequn8128/flink

flink-python/pyflink/fn_execution/coder_impl.py
@@ -79,6 +79,30 @@ class RowCoderImpl(StreamCoderImpl):
         return 'RowCoderImpl[%s]' % ', '.join(str(c) for c in self._field_coders)
 
 
+class ArrayCoderImpl(StreamCoderImpl):
+
+    def __init__(self, elem_coder):
+        self._elem_coder = elem_coder
+
+    def encode_to_stream(self, value, out_stream, nested):
+        out_stream.write_bigendian_int32(len(value))
+        for elem in value:
+            if elem is None:
+                out_stream.write_byte(False)
+            else:
+                out_stream.write_byte(True)
+                self._elem_coder.encode_to_stream(elem, out_stream, nested)
+
+    def decode_from_stream(self, in_stream, nested):
+        size = in_stream.read_bigendian_int32()
+        elements = [self._elem_coder.decode_from_stream(in_stream, nested)
+                    if in_stream.read_byte() else None for _ in range(size)]
+        return elements
+
+    def __repr__(self):
+        return 'ArrayCoderImpl[%s]' % repr(self._elem_coder)
+
+
 class BigIntCoderImpl(StreamCoderImpl):
     def encode_to_stream(self, value, out_stream, nested):
         out_stream.write_bigendian_int64(value)